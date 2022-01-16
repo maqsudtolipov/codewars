a82b5d51702adb202d36be4e8dcc1e57ea35d5ba
Maqsud <maqsudtolipov9@gmail.com>
2022-01-16 19:49:10 +0500

    Update Sumofpositive.js

--- 8-kyu/Sumofpositive.js
+++ 8-kyu/Sumofpositive.js
@@ -1,5 +1,6 @@
 function positiveSum(arr) {
-  return arr.map(i => Math.abs(i));
+  return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
 }
 
-console.log(positiveSum([1, -2, 3, 4, -5]));
+// test
+console.log(positiveSum([1, -2, 3, 4, 5]));
