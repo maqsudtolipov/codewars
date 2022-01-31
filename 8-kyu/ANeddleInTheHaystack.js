dba240f8b0040ae1848b5ba1721037d1df5445ad
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-01-31 17:24:08 +0500

    Create ANeddleInTheHaystack.js

---
+++ 8-kyu/ANeddleInTheHaystack.js
@@ -0,0 +1,19 @@
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`;
+}
+
+// test
+console.log(
+  findNeedle([
+    '3',
+    '123124234',
+    undefined,
+    'needle',
+    'world',
+    'hay',
+    2,
+    '3',
+    true,
+    false,
+  ])
+);
